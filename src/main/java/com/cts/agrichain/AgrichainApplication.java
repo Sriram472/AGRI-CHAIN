99ae36d15aeed2030bc9c7c16d2da0c14648bf53
Sriram <2478382@cognizant.com>
2026-03-26 16:37:08 +0530

    Initial project commit

---
+++ src/main/java/com/cts/agrichain/AgrichainApplication.java
@@ -0,0 +1,13 @@
+package com.cts.agrichain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrichainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrichainApplication.class, args);
+    }
+
+}
