9ceeea90ab9c43851f678090a68bc9644a9d1699
Sriram <2478382@cognizant.com>
2026-04-01 11:07:57 +0530

    Added complete project files

--- src/main/java/com/cts/agrichain/AgrichainApplication.java
+++ src/main/java/com/cts/agrichain/AgrichainApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AgrichainApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
         SpringApplication.run(AgrichainApplication.class, args);
     }
 
